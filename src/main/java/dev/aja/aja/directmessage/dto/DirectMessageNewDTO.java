5f95b58e3957bbe8da8c18a81c95ec49fc7a4b98
Mel0nABC <alex@alex.es>
2026-05-11 15:21:37 +0200

    Add builder annotation

--- src/main/java/dev/aja/aja/directmessage/dto/DirectMessageNewDTO.java
+++ src/main/java/dev/aja/aja/directmessage/dto/DirectMessageNewDTO.java
@@ -1,8 +1,11 @@
 package dev.aja.aja.directmessage.dto;
 
+import lombok.Builder;
+
 /**
  * Record/DTO para enviar nuevos mensajes
  */
+@Builder
 public record DirectMessageNewDTO(
                 Long idUserTo,
                 String text) {
